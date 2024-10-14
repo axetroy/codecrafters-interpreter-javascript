abcfa5b9bb04b42b581bbf4d607e85b87276e058
Axetroy <axetroy.dev@gmail.com>
2024-10-14 09:53:36 +0800

    update

--- app/main.js
+++ app/main.js
@@ -19,18 +19,20 @@ const filename = args[1];
 const fileContent = fs.readFileSync(filename, "utf8");
 
 if (fileContent.length !== 0) {
-  fileContent.split(/\s+/).forEach((token) => {
-    switch (token) {
-      case "(": 
-        console.log("LEFT_PAREN", "(", null);
-        break;
-      case ")":
-        console.log("RIGHT_PAREN", ")", null);
-        break;
-    }
-  });
+  for (const line of fileContent.split(/\s+/)) {
+    for (const char of line) {
+      switch (char) {
+        case "(":
+          console.log("LEFT_PAREN ( null");
+          break;
+        case ")":
+          console.log("RIGHT_PAREN ) null");
+          break;
+      }
 
-  console.log("EOF", null);
+    }
+  }
+  console.log("EOF  null");
 } else {
   console.log("EOF  null");
 }
